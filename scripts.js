a268702da3ad3c968ed3a27ad4290975bc2c9899
Nana Boateng <nanakab.dev@gmail.com>
2026-07-13 01:45:42 +0000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,56 @@
+const body = document.querySelector("body");
+const pictures = document.querySelectorAll(".pic");
+
+const modalTemplate = document.querySelector(".pic-modal-template");
+const templateClone = modalTemplate.content.cloneNode(true);
+document.body.appendChild(templateClone);
+
+const picModal = document.querySelector(".pic-modal");
+const modalImage = document.querySelector(".modal-image");
+
+const closeBtn = document.querySelector(".close-btn");
+const previousBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let currentImageIndex = 0;
+
+
+pictures.forEach((picture, index) => {
+
+        picture.addEventListener("click", () =>{
+
+        currentImageIndex = index;
+
+        picModal.showModal();
+        modalImage.src = picture.src;
+        console.log("you clicked a picture");
+    });
+});
+
+
+ // move to the next image
+nextBtn?.addEventListener("click", () => {
+    currentImageIndex++;
+
+    if (currentImageIndex >= pictures.length)  { currentImageIndex =  0; }
+
+     modalImage.src = pictures[currentImageIndex].src;
+  
+});
+
+
+
+
+
+closeBtn?.addEventListener("click", (event) => { picModal.close(); })
+
+
+    // move to the previous image
+previousBtn?.addEventListener("click", () => {
+
+    currentImageIndex --
+
+    if (currentImageIndex < 0){ currentImageIndex = pictures.length - 1; }  
+    
+    modalImage.src = pictures[currentImageIndex].src;
+})
